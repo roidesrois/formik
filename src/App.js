6d3d67c3b5843a1db5cc0fe32ed504b34d40c094
Ruslan Salimov <rsalimov@rcom.az>
2018-11-30 14:08:48 +0400

    Add field array

--- src/App.js
+++ src/App.js
@@ -1,56 +1,56 @@
 // eslint:ignore
-import React from 'react';
-import { Switch, Link, Route } from 'react-router-dom';
-import MiniFormik from './examples/MiniFormik';
-import Basic from './examples/Basic';
-import Array from './examples/Array';
-import Fieldset from './examples/Fieldset';
-import './css/Example.css';
-import './css/RichEditor.css';
-import { COLORS } from './theme';
+import React from "react";
+import { Switch, Link, Route } from "react-router-dom";
+import MiniFormik from "./examples/MiniFormik";
+import Basic from "./examples/Basic";
+import { Invitation } from "./examples/Array";
+import Fieldset from "./examples/Fieldset";
+import "./css/Example.css";
+import "./css/RichEditor.css";
+import { COLORS } from "./theme";
 
 const ExampleRoute = ({ children, color, ...props }) => (
   <Route
     {...props}
     render={() => (
       <div
-        style={{ height: '100%' }}
+        style={{ height: "100%" }}
         className={`formik-example formik-example--${color}`}
       >
         <Link
           to="/"
           style={{
-            position: 'absolute',
+            position: "absolute",
             bottom: 20,
             left: 16,
-            color: '#000',
-            fontWeight: '800',
-            padding: '.5rem 1rem',
+            color: "#000",
+            fontWeight: "800",
+            padding: ".5rem 1rem",
             borderRadius: 4,
-            background: '#fff',
-            boxShadow: '0 4px 8px rgba(0,0,0,.2)',
+            background: "#fff",
+            boxShadow: "0 4px 8px rgba(0,0,0,.2)"
           }}
         >
           <span aria-label="finger" role="img">
             👈
-          </span>{' '}
+          </span>{" "}
           Back
         </Link>
         <div
           style={{
-            height: '100%',
+            height: "100%",
             background: COLORS[color][5],
-            padding: '4rem 2rem',
+            padding: "4rem 2rem"
           }}
         >
           <div
             style={{
-              borderRadius: '4px',
-              boxShadow: '0 8px 16px rgba(0,0,0,.2)',
-              background: '#fff',
+              borderRadius: "4px",
+              boxShadow: "0 8px 16px rgba(0,0,0,.2)",
+              background: "#fff",
               maxWidth: 400,
-              margin: '0 auto',
-              padding: '2rem',
+              margin: "0 auto",
+              padding: "2rem"
             }}
           >
             {children}
@@ -68,24 +68,24 @@ export default function App() {
         path="/"
         exact
         render={() => (
-          <div style={{ height: '100%', background: '#666ee8' }}>
+          <div style={{ height: "100%", background: "#666ee8" }}>
             <div
               style={{
-                margin: '0 auto',
+                margin: "0 auto",
                 maxWidth: 600,
-                fontSize: '2rem',
+                fontSize: "2rem",
                 fontWeight: 800,
-                padding: '1rem',
-                lineHeight: 1.6,
+                padding: "1rem",
+                lineHeight: 1.6
               }}
             >
               <h1
                 style={{
-                  color: '#fff',
+                  color: "#fff",
                   fontWeight: 800,
-                  letterSpacing: '-1px',
-                  marginBottom: '2rem',
-                  lineHeight: 1,
+                  letterSpacing: "-1px",
+                  marginBottom: "2rem",
+                  lineHeight: 1
                 }}
               >
                 Formik Demos
@@ -93,54 +93,54 @@ export default function App() {
 
               <Link
                 to="/miniformik"
-                style={{ display: 'block', color: '#fff' }}
+                style={{ display: "block", color: "#fff" }}
               >
                 <span role="img" aria-label="baby">
                   👶
-                </span>{' '}
+                </span>{" "}
                 {`<MiniFormik />`}
               </Link>
-              <Link to="/basic" style={{ display: 'block', color: '#fff' }}>
+              <Link to="/basic" style={{ display: "block", color: "#fff" }}>
                 <span role="img" aria-label="rocket">
                   🚀
-                </span>{' '}
+                </span>{" "}
                 The Basics
               </Link>
-              <Link to="/fieldset" style={{ display: 'block', color: '#fff' }}>
+              <Link to="/fieldset" style={{ display: "block", color: "#fff" }}>
                 <span role="img" aria-label="build">
                   🛠
-                </span>{' '}
+                </span>{" "}
                 {`Building your own <Fieldset/>`}
               </Link>
-              <Link to="/array" style={{ display: 'block', color: '#fff' }}>
+              <Link to="/array" style={{ display: "block", color: "#fff" }}>
                 <span role="img" aria-label="fire">
                   🔥
-                </span>{' '}
+                </span>{" "}
                 Nested state + Arrays
               </Link>
               <div
                 style={{
-                  fontSize: '1.2rem',
-                  marginTop: '4rem',
+                  fontSize: "1.2rem",
+                  marginTop: "4rem"
                 }}
               >
                 <a
                   href="https://jaredpalmer.com/formik"
-                  style={{ color: '#fff', display: 'block' }}
+                  style={{ color: "#fff", display: "block" }}
                 >
                   Docs
                 </a>
-                {'  '}
+                {"  "}
                 <a
                   href="https://github.com/jaredpalmer/formik"
-                  style={{ color: '#fff', display: 'block' }}
+                  style={{ color: "#fff", display: "block" }}
                 >
                   GitHub
                 </a>
-                {'  '}
+                {"  "}
                 <a
                   href="https://twitter.com/jaredpalmer"
-                  style={{ color: '#fff', display: 'block' }}
+                  style={{ color: "#fff", display: "block" }}
                 >
                   @jaredpalmer
                 </a>
@@ -156,7 +156,7 @@ export default function App() {
         <Basic />
       </ExampleRoute>
       <ExampleRoute path="/array" exact color="green">
-        <Array />
+        <Invitation />
       </ExampleRoute>
       <ExampleRoute path="/fieldset" exact color="blue">
         <Fieldset />
--- src/examples/Array.js
+++ src/examples/Array.js
@@ -1,112 +1,140 @@
-import React from 'react';
-import { Formik, Field, Form, FieldArray, ErrorMessage } from 'formik';
-import { Debug } from '../components/Debug';
-import * as Yup from 'yup';
-import { EditorState } from 'draft-js';
-import { RichEditorExample } from '../components/RichEditor';
+import React, { Fragment, Component } from "react";
+import { Formik, Field, Form, FieldArray, ErrorMessage } from "formik";
+import { Debug } from "../components/Debug";
+import * as Yup from "yup";
 
 const initialValues = {
-  friends: [
+  groups: [
     {
-      name: '',
-      email: '',
-    },
-  ],
-  editorState: new EditorState.createEmpty(),
+      // group
+      name: "Group Name",
+      fields: [
+        // group fields
+        { name: "", type: "text", placeholder: "firstName" },
+        { name: "", type: "text", placeholder: "lastName" },
+        { name: "", type: "email", placeholder: "email" }
+      ]
+    }
+  ]
 };
 
-const Invitation = () => (
-  <div>
-    <h1>Invite friends</h1>
-    <Formik
-      initialValues={initialValues}
-      validationSchema={Yup.object({
-        friends: Yup.array().of(
-          Yup.object({
-            name: Yup.string().required('Required'),
-            email: Yup.string()
-              .email('Invalid email!')
-              .required('Required'),
-          })
-        ),
-      })}
-      onSubmit={values => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-        }, 500);
-      }}
-    >
-      {({ values, isSubmitting, handleBlur, setFieldValue }) => (
-        <Form>
-          <RichEditorExample
-            editorState={values.editorState}
-            onChange={setFieldValue}
-            onBlur={handleBlur}
-          />
-          <FieldArray name="friends">
-            {({ push, remove }) => (
-              <React.Fragment>
-                {values.friends &&
-                  values.friends.length > 0 &&
-                  values.friends.map((friend, index) => (
-                    <div className="row">
-                      <div className="col">
-                        <Field name={`friends[${index}].name`}>
-                          {({ field, form }) => (
-                            <input
-                              {...field}
-                              type="text"
-                              placeholder="Jane Doe"
-                            />
-                          )}
-                        </Field>
-                        <ErrorMessage name={`friends[${index}].name`}>
-                          {msg => <div className="field-error">{msg}</div>}
-                        </ErrorMessage>
-                      </div>
-                      <div className="col">
-                        <Field
-                          name={`friends[${index}].email`}
-                          type="email"
-                          placeholder="jane@example.com"
-                        />
-                        <ErrorMessage name={`friends[${index}].email`}>
-                          {msg => <div className="field-error">{msg}</div>}
-                        </ErrorMessage>
-                      </div>
-                      <div className="col">
-                        <button type="button" onClick={() => remove(index)}>
-                          X
-                        </button>
-                      </div>
-                    </div>
-                  ))}
-                <button
-                  type="button"
-                  onClick={() => push({ name: '', email: '' })}
-                  className="secondary"
-                >
-                  Add Friend
-                </button>
-              </React.Fragment>
-            )}
-          </FieldArray>
-          <button type="submit" disabled={isSubmitting}>
-            Invite
-          </button>
-          <Debug />
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+// console.log(initialValues.pages[0].groups[0].fields[0].placeholder);
 
-export default Invitation;
+export class Invitation extends Component {
+  state = {
+    pages: [
+      {
+        // page
+        step: "Page Number",
+        groups: [
+          {
+            // group
+            name: "Group Name",
+            fields: [
+              // group fields
+              { name: "", type: "text", placeholder: "firstName" },
+              { name: "", type: "text", placeholder: "lastName" },
+              { name: "", type: "email", placeholder: "email" }
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  componentDidMount() {
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Invite group</h1>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={Yup.object({
+            groups: Yup.array().of(
+              Yup.object({
+                "groups.0.fields.0.name": Yup.string().required("Required"),
+                "groups.0.fields.2.name": Yup.string()
+                  .email("Invalid email!")
+                  .required("Required")
+              })
+            )
+          })}
+          onSubmit={values => {
+            console.log(values);
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+            }, 500);
+          }}
+        >
+          {({ values, isSubmitting, handleBlur, setFieldValue }) => (
+            <Form>
+              <FieldArray name="groups">
+                {({ push, remove }) => (
+                  <Fragment>
+                    {values.groups.map((group, groupIdx) => (
+                      <div className="row" key={groupIdx}>
+                        {group.fields &&
+                          group.fields.length > 0 &&
+                          group.fields.map((field, fieldIdx) => (
+                            <div className="col" key={fieldIdx}>
+                              {console.log(field.placeholder)}
+                              <Field
+                                name={`groups.${groupIdx}.fields.${fieldIdx}.name`}
+                                type={field.type}
+                                placeholder={field.placeholder}
+                              />
+                              <ErrorMessage
+                                name={`groups.${groupIdx}.fields.${fieldIdx}.name`}
+                              >
+                                {msg => (
+                                  <div className="field-error">{msg}</div>
+                                )}
+                              </ErrorMessage>
+                            </div>
+                          ))}
+                        <div className="col">
+                          <button
+                            type="button"
+                            onClick={() => remove(groupIdx)}
+                          >
+                            X
+                          </button>
+                        </div>
+                        <div className="col">
+                          {console.log(values.groups[0])}
+                          <button
+                            type="button"
+                            onClick={() => push(values.groups[0])}
+                            // className="secondary"
+                          >
+                            +
+                          </button>
+                        </div>
+                      </div>
+                    ))}
+                  </Fragment>
+                )}
+              </FieldArray>
+              <button type="submit" disabled={isSubmitting}>
+                Invite
+              </button>
+              <Debug />
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
+}
+// export default Invitation;
 
 // editorState: new EditorState.createEmpty(),
 
 // Yup.object({
-//   friends: Yup.array().of(
+//   group0: Yup.array().of(
 //     Yup.object({
 //       name: Yup.string().required('Required'),
 //       email: Yup.string()
